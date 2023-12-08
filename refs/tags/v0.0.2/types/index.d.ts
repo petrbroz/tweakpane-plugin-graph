9b36783cc4a4b63f34d8850894ebb9027d6ee1e3
Petr Broz <petr.broz@autodesk.com>
2023-12-08 11:59:03 +0100

    Updated gh-pages.

--- refs/tags/v0.0.2/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const id = "graph2-plugin-bundle";
-export declare const css = "__css__";
-export declare const plugins: import("@tweakpane/core").MonitorBindingPlugin<number, import("./plugin.js").GraphPluginParams>[];
